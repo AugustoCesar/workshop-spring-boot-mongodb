4d4843f15e5a3a6aa96de24219b17e9f7c432162
Augusto Nunes <augustocesar.udi@gmail.com>
2024-02-02 12:11:21 -0300

    Get user by id

--- src/main/java/com/augustonunes/workshopmongo/services/UserService.java
+++ src/main/java/com/augustonunes/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.augustonunes.workshopmongo.services;
 
 import com.augustonunes.workshopmongo.domain.User;
 import com.augustonunes.workshopmongo.repositories.UserRepository;
+import com.augustonunes.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,13 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> userOpt = userRepository.findById(id);
+        if (userOpt.isEmpty()) {
+            throw new ObjectNotFoundException("Object not found");
+        }
+        return userOpt.get();
     }
 }

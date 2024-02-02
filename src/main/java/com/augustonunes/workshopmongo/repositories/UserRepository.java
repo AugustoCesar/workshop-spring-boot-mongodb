992330d3077a16733a2650c2d91947b2bc1ba93f
Augusto Nunes <augustocesar.udi@gmail.com>
2024-02-02 11:23:29 -0300

    Connecting MongoDB, service, repository

---
+++ src/main/java/com/augustonunes/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.augustonunes.workshopmongo.repositories;
+
+import com.augustonunes.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/augustonunes/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.augustonunes.workshopmongo.services;
+
+import com.augustonunes.workshopmongo.domain.User;
+import com.augustonunes.workshopmongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}

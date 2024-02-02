bb159f0f63874754133b1324257e8a05bc1e99d8
Augusto Nunes <augustocesar.udi@gmail.com>
2024-02-02 15:34:28 -0300

    Post by id

---
+++ src/main/java/com/augustonunes/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.augustonunes.workshopmongo.services;
+
+import com.augustonunes.workshopmongo.domain.Post;
+import com.augustonunes.workshopmongo.repositories.PostRepository;
+import com.augustonunes.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+
+    public Post findById(String id) {
+        Optional<Post> postOpt = postRepository.findById(id);
+        return postOpt.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+}

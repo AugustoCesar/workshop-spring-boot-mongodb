724ccb873a8380e9f80d1a5884a53b4b45a7def7
Augusto Nunes <augustocesar.udi@gmail.com>
2024-02-02 15:55:17 -0300

    Query methods consult

--- src/main/java/com/augustonunes/workshopmongo/services/PostService.java
+++ src/main/java/com/augustonunes/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.augustonunes.workshopmongo.services.exception.ObjectNotFoundException
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +19,10 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> postOpt = postRepository.findById(id);
         return postOpt.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 
 }
